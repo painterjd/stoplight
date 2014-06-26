9d0f0828573e30e76410f5ecd1e29046d8364b01
Jamie Painter <jamie@jpainter.org>
2014-06-26 07:57:16 -0400

    Adds many missing files

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+REQUIRES = ['six']
+
+setup(
+    name='stoplight',
+    version='0.1',
+    description='Input validation framework for Python',
+    author='Jamie Painter',
+    author_email='jamie.painter@rackspace.com',
+    install_requires=REQUIRES,
+    test_suite='stoplight',
+    zip_safe=False,
+    include_package_data=True,
+    packages=find_packages(exclude=['ez_setup'])
+)
