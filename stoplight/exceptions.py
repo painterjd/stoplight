b79ae018a0dd05998b54a990c2e782aaea5eeb6f
BenjamenMeyer <ben.meyer@rackspace.com>
2018-04-06 14:31:15 -0400

    Bug Fix: ValueError on message format
    - Bug Fix: The stoplight Exceptions were provided a string that
      contained a single brace ({ or }) then it was the `msg.format()`
      line would cause a ValueError to improperly occur.

--- stoplight/exceptions.py
+++ stoplight/exceptions.py
@@ -1,10 +1,17 @@
+import warnings
 
 
 class ValidationFailed(ValueError):
     """User input was inconsistent with API restrictions"""
 
     def __init__(self, msg, *args, **kwargs):
-        msg = msg.format(*args, **kwargs)
+        if len(args) or len(kwargs):
+            warnings.warn(
+                'It is recommended to format the Exception message '
+                'outside the parameters to the Exception.',
+                DeprecationWarning
+            )
+            msg = msg.format(*args, **kwargs)
         super(ValidationFailed, self).__init__(msg)
 
 
@@ -12,5 +19,12 @@ class ValidationProgrammingError(ValueError):
     """Caller did not map validations correctly"""
 
     def __init__(self, msg, *args, **kwargs):
-        msg = msg.format(*args, **kwargs)
+        if len(args) or len(kwargs):
+            warnings.warn(
+                'It is recommended to format the Exception message '
+                'outside the parameters to the Exception.',
+                DeprecationWarning
+            )
+            msg = msg.format(*args, **kwargs)
+            msg = msg.format(*args, **kwargs)
         super(ValidationProgrammingError, self).__init__(msg)
